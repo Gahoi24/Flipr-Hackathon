50853757ed66cb0539c9dcdabe54e2e37aef22d9
Krishna Agarwal <Krishna_a@ce.iitr.ac.in>
2022-02-11 23:46:31 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,14 @@
 const express=require('express')
+const morgan=require('morgan')
+
 const app=express()
 const port=5000
 
+app.use(morgan('dev'));
+
+app.get('/',(req,res)=>{
+    res.send("Hello World");
+});
 app.listen(port,async()=>{
     console.log("Server started at Port " + port);
 })
